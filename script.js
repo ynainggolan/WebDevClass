d02a17b64d1d6a5f22dad21219dc1b15a5facdf4
ynainggolan <yhpnainggolan21@gmail.com>
2022-09-28 13:09:33 -0400

    added javascript

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function changeColor() {
 	let color = document.body.style.color;
 	if (color === 'blue') {
-		document.body.style.color = 'red';
+		document.body.style.color = 'white';
 	} else {
 		document.body.style.color = 'blue';
 	}
